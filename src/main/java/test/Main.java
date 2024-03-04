facc49a6b72401450214a7d1abc71713ec2a1d8e
KabsiMontassar <kebsimontassar@gmail.com>
2024-03-04 13:17:49 +0100

    c

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,10 +1,21 @@
 package test;
 
+import com.mailjet.client.errors.MailjetException;
+import services.JavaMailJett;
+import services.ValidateEmail;
+
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MailjetException {
+        String sender = "playmatepidev@gmail.com";
+        String Receipient = "aziztaraji1@gmail.com";
+        String contenue = "sa77a el 0  git ya zekri";
+
+        JavaMailJett.send(Receipient,sender,contenue);
+
+
 
     }
 }
