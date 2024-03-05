69faaf7260f483896fb048f0ed18e4071da53475
malouk30 <malek.benjemia@esprit.tn>
2024-03-05 23:30:45 +0100

    update

--- src/main/java/test/Controllers/TerrainController/statController.java
+++ src/main/java/test/Controllers/TerrainController/statController.java
@@ -13,7 +13,6 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import services.GestionTerrain.AvisService;
 import models.Terrain;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -22,28 +21,23 @@ import java.sql.SQLException;
 import java.util.List;
 import javafx.scene.control.Button;
 import test.MainFx;
-
+//*******************************************************************************************
 public class statController {
 
     @FXML
     private BarChart<String, Number> barChart;
-
     @FXML
     private CategoryAxis xAxis;
-
     @FXML
     private NumberAxis yAxis;
-
     private AvisService avisService;
-
     @FXML
     private Button btretour;
-
-
+    //*******************************************************************************************
     public statController() {
         this.avisService = new AvisService();
     }
-
+    //*******************************************************************************************
     @FXML
     public void initialize() throws IOException {
         List<Terrain> terrains;
@@ -51,29 +45,21 @@ public class statController {
             terrains = avisService.getTerrainsOrderByCommentaires();
             XYChart.Series<String, Number> series = new XYChart.Series<>();
             for (Terrain terrain : terrains) {
-                series.getData().add(new XYChart.Data<>(terrain.getNomTerrain(), terrain.getNbCommentaires()));
-            }
+                series.getData().add(new XYChart.Data<>(terrain.getNomTerrain(), terrain.getNbCommentaires()));}
             barChart.getData().add(series);
-
             // Formatage des valeurs de l'axe des ordonnées en entiers sans virgule
             yAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(yAxis) {
                 @Override
                 public String toString(Number object) {
                     return String.format("%d", object.intValue());
-                }
-            });
+                }});
             // Définir l'incrémentation de l'axe des ordonnées a 1
             yAxis.setTickUnit(1);
             // Définir la valeur de départ de l'axe des ordonnées a 0
             yAxis.setLowerBound(0);
             // Désactiver la génération automatique des étiquettes
-            yAxis.setAutoRanging(false);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+            yAxis.setAutoRanging(false);} catch (SQLException e) {e.printStackTrace();}}
+    //*******************************************************************************************
     @FXML
     void exporterEnExcel(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
@@ -84,16 +70,9 @@ public class statController {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 writer.write("Terrain           Commentaires\n");
                 for (XYChart.Data<String, Number> data : barChart.getData().get(0).getData()) {
-                    writer.write(data.getXValue() + "           " + data.getYValue() + "\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-
+                    writer.write(data.getXValue() + "           " + data.getYValue() + "\n");}
+            } catch (IOException e) {e.printStackTrace();}}}
+    //*******************************************************************************************
     @FXML
     void retour(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(MainFx.class.getResource("GestionTerrain/PageTerrain.fxml"));
@@ -103,5 +82,4 @@ public class statController {
         stage.setScene(new Scene(root));
         stage.show();
         // Récupérer la fenêtre actuelle et la cacher
-        ((Stage) btretour.getScene().getWindow()).hide();}
-    }
+        ((Stage) btretour.getScene().getWindow()).hide();}}
