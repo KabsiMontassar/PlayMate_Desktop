29982de3e82b022680d30158f4df6730c95aa505
aziz-benzekri <Mohamedaziz.Benzekri@esprit.tn>
2024-03-05 23:29:06 +0100

    choicebox reser

--- src/main/java/test/MainFx.java
+++ src/main/java/test/MainFx.java
@@ -10,7 +10,7 @@ public class MainFx extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainFx.class.getResource("GestionReservation/jeuPlayMate.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainFx.class.getResource("GestionReservation/choix2.fxml"));  //GestionReservation/jeuPlayMate.fxml
 
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
 
