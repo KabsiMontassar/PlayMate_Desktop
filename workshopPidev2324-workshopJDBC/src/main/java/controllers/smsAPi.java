8c0f42c6314b9dc8ef94b5e56de74310e2f9d006
malouk30 <100469136+malouk30@users.noreply.github.com>
2024-03-12 09:59:38 +0100

    Update smsAPi.java

--- workshopPidev2324-workshopJDBC/src/main/java/controllers/smsAPi.java
+++ workshopPidev2324-workshopJDBC/src/main/java/controllers/smsAPi.java
@@ -6,15 +6,15 @@ import com.twilio.type.PhoneNumber;
 //*******************************************************************************************
 public class smsAPi {
 
-    public static final String ACCOUNT_SID = "AC3b86181a71ed65d3d7b62c900fa844a0";
-    public static final String AUTH_TOKEN = "cc3cc54215e06d8e4e04a12b73e33055";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     static {Twilio.init(ACCOUNT_SID, AUTH_TOKEN);}
     public static void sendSms(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new PhoneNumber("+21626564986"), // verifie
-                new PhoneNumber("+19497102455"),//virtuel
+                new PhoneNumber("+"), // verifie
+                new PhoneNumber("+"),//virtuel
                 "Vous avez reçu un nouvel avis sur votre terrain"
         ) .create();
         System.out.println("SMS Sent with SID: " + message.getSid());}}
